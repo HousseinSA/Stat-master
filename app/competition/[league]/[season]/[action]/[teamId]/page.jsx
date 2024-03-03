e320422df468b2e91b6b5285b52bc7d781fad517
houssein sedoum <nejihoussein1@gmail.com>
2024-03-03 02:31:00 +0000

    fix with TeamId page fetching data

--- app/competition/[league]/[season]/[action]/[teamId]/page.jsx
+++ app/competition/[league]/[season]/[action]/[teamId]/page.jsx
@@ -1,11 +1,9 @@
 import TeamContainer from "@/components/mainSection/CompetitionData/TeamsContainer/TeamContainer";
-import {
-  getTeamData,
-  getTeamMatches,
-} from "@/utils/allDataFetchingFunctions";
+import { getTeamData, getTeamMatches } from "@/utils/allDataFetchingFunctions";
 const TeamPage = async ({ params }) => {
   const { teamId, action } = params;
 
+  // eslint-disable-next-line
   const [data, matches] = await Promise.all([
     getTeamData(action, teamId),
     getTeamMatches(action, teamId),
